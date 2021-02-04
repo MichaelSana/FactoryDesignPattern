dda51df8cdb63c21de251a01329137f5a25f2e6f
Michael Sana <msana@email.sc.edu>
2021-02-03 20:54:26 -0500

    First save

---
+++ Bike.java
@@ -0,0 +1,13 @@
+public abstract class Bike{
+    protected String name;
+    protected Double price;
+    protected int numWheels;
+    protected boolean hasPeddals;
+    protected boolean hasTrainingWheels;
+
+    public void createBike(){
+
+    }
+    
+
+}
